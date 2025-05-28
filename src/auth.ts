f8c19434142954cde6d4aba8b7a48f5957c4cef4
MAC <MAC@192.168.101.93.non-exists.ptr.local>
2025-05-29 01:21:55 +0700

    gửi api đăng nhập đến backend

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import { InactiveAccountError, InvalidEmailPasswordError } from "./utils/errors"
+import { sendRequest } from "./utils/api"
+import { IUser } from "./types/next-auth"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -11,34 +14,54 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        console.log (">>> check credientials:", credentials)
-        let user = null
- 
-        // logic to salt and hash password
-        // call backend
-        // user = {
-        //   _id: '123',
-        //   username: '123',
-        //   email: '123',
-        //   isVerify: '123',
-        //   type: '123',
-        //   role: '123',
-        // }
- 
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // Optionally, this is also the place you could do a user registration
-          throw new Error("Invalid credentials.")
+        const res = await sendRequest<IBackendRes<ILogin>>({
+          method: "POST",
+          url: "http://localhost:8080/api/AnhThi1/auth/login",
+          body: {
+            username: credentials.email,
+            password: credentials.password,
+          }
+      })
+
+      console.log (">>> check res", res)
+      // login thành công
+      if (!res.statusCode){
+        return {
+          _id: res.data?.user?._id,
+          name: res.data?.user?.name,
+          email: res.data?.user?.email,
+          accessToken: res.data?.accessToken,
         }
+      // sai mật khẩu
+      }else if (+res.statusCode === 401){
+        throw new InvalidEmailPasswordError();
+      } 
+       // chưa kích hoạt tài khoản
+       else if (+res.statusCode === 400){
+        throw new InactiveAccountError();
+      } else{
+        throw new Error("Internal server error");
+      }
  
         // return user object with their profile data
-        console.log(">>> check user:", user)
-        return user
-        
+        return res.data
       },
     }),
   ],
+  
   pages: {
     signIn: "/auth/login",
+  },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.user = user as IUser;
+      }
+      return token
+    },
+    session({ session, token }) {
+      (session.user as IUser) = token.user;
+      return session
+    },
   },
 })
